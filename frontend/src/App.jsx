04ee73dac296997512a1dd1152499636a93c8672
Raffael Pimentel Ribeiro <ribeiro_pimentel@discente.ufg.br>
2022-08-19 21:12:48 -0300

    adicionando pasta front

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import  React from 'react'
+function App() {
+
+  return (
+   <>
+    <div>
+        <h1>FULLSTACK - PROJETO</h1>
+    </div>
+    <div>
+        <h2>FRONT END</h2>
+    </div>
+   </>
+  )
+}
+
+export default App
